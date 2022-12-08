6cb15ea620d052625d6df29d8d03fe889f9da222
20083732 <k20083732@kcl.ac.uk>
2022-12-08 22:21:49 +0000

    Update employee.py

--- employee.py
+++ employee.py
@@ -4,12 +4,68 @@
 class Employee:
     def __init__(self, name):
         self.name = name
+        self.contract = contract
+        self.commission = commission
 
     def get_pay(self):
-        pass
+        pay = self.contract.get_pay()
+        if self.commission:
+            pay += self.commission.get_pay()
+        return pay
 
     def __str__(self):
-        return self.name
+        string =f'{self.name} works on {str{self.contract}}'
+        if self.commission:
+            string += f' and receives{str{self.commission}}'
+        string +=f'. Their total pay is {self.get_pay()}.'
+        return string
+
+class HourlyContract:
+    def __init__(self,hourly_pay,hours):
+        self.hourly_pay = hourly_pay
+        self.hours = hours
+
+    def get_pay(self):
+        return self.hourly_pay*self.hours
+
+    def __str__(self):
+        string = f'a contract of {self.hours} hours at {self.hourly_pay}/hour'
+        return string
+
+class SalaryContract:
+    def __init__(self,salary):
+        self.salary = salary
+
+    def get_pay(self):
+        return self.salary
+
+    def __str__(self):
+        string = f'a monthly salary of {self.salary}'
+        return string
+
+class BonusCommission:
+    def __init__(self, bonus):
+        self.bonus = bonus
+
+    def get_pay(self):
+        return self.bonus
+
+    def __str__(self):
+        string = f'a bonus of commission of {self.bonus}'
+        return string
+
+class ContractCommission:
+    def __init__(self, contract=0, commission=200):
+        self.contract = contract
+        self.commission = commission
+
+    def get_pay(self):
+        return self.contract*self.commission
+
+    def __str__(self):
+        string = f'a commission for {self.contract} contract(s) at {self.commission}/hour'
+        return string
+
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
